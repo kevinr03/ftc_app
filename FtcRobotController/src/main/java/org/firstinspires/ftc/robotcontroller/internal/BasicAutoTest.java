6afaa9ade8dbb335a5ac43be5b0522bb4e115f2a
kevinr03 <kevin.n.rockwell@gmail.com>
2019-02-23 16:58:48 -0800

    reimplement scan handing code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/BasicAutoTest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/BasicAutoTest.java
@@ -14,8 +14,27 @@ public class BasicAutoTest extends BaseAuto {
         waitForStart();
         startAuto();
         stopDrive();
-        scanMineral(5000);
-        //driveInches(44, .6);
+        int goldPos = scanMineral(3000);
 
+        telemetry.addData("GoldPos = ", goldPos);
+        telemetry.update();
+        if (goldPos == -1 || goldPos == -2) {
+            //Gold Position Left
+            driveInches(1, 1);
+            runToPos(getTicks(14.5), -getTicks(14.5), 1);
+            sleep(500);
+            driveInches(40, .6);
+        }
+        else if (goldPos == 0) {
+            //Gold Position Center
+            runToPos(getTicks(1.17), 0, .6);
+            driveInches(40, .6);
+        }
+        else if (goldPos == 1) {
+            //Gold Position Left
+            driveInches(1, 1);
+            runToPos(0, getTicks(20), 1);
+            driveInches(40, .6);
+        }
     }
 }
